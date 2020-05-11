417a8f1c9731e4e3926552bbbf71a2b516b72f29
malcolm staso <mstaso1@gmail.com>
2020-05-11 15:33:23 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -4,12 +4,12 @@ end
 
 def find_min_value(array)
   count = 0
-  max_value = -1
+  min_value = -1
   while count < array.length do
-    if max_value < array[count]
-      max_value = array[count]
+    if min_value > array[count]
+      min_value = array[count]
     end  
     count += 1
   end
-  max_value
+  min_value
 end
